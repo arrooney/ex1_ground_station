564400963e11df8dfcdf6b3ac206a82aeea37dc8
Brendan Bruner <bbruner@ualberta.ca>
2017-04-25 12:23:44 -0600

    Was able to modify pforth make file in pforth/build/unix to build a static library of the pforth kernal. Compile using make testlib

--- logs/view.sh
+++ logs/view.sh
@@ -1,3 +1,7 @@
+# ./view.sh - view entire output_log in terminal
+# ./view.sh x - view last x lines of output_log in terminal
+#	./view.sh 20 - shows the last 20 lines of the output_log
+
 if [ -z $1 ]
 then
     cat output_log.txt
