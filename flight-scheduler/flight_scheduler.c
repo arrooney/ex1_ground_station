67c1914f14a96835a26744398ab112010878fab4
Brendan Bruner <bbruner@ualberta.ca>
2018-02-23 17:24:26 -0700

    source code in folder flight-scheduler makes a flight schedule compatable with ADCS update code

--- flight-scheduler/flight_scheduler.c
+++ flight-scheduler/flight_scheduler.c
@@ -21,59 +21,64 @@ Note that the maximum number of commands per file is 250. The length of an orbit
 #include <stdio.h>
 #include <stdint.h>
 #include <string.h>
+#include <inttypes.h>
 #include <endian.h>
 
 #define SCRIPT_MAX_LENGTH (4*20+3)
-#define MAX_COMMANDS_PER_SCHEDULE (4*30+3)
+#define MAX_COMMANDS_PER_SCHEDULE (4*60+3)
 
-struct command_t __ATTRIBUTE__((packed))
+struct command_t 
 {
 	uint32_t unix_time;
 	uint8_t command_length;
 	char command[SCRIPT_MAX_LENGTH];
-} command_t;
+} __attribute__((packed));
 
-struct schedule_t __ATTRIBUTE__((packed))
+struct schedule_t
 {
 	uint8_t total_commands;
-	command_t slot[MAX_COMMANDS_PER_SCHEDULE];
-};
+	struct command_t slot[MAX_COMMANDS_PER_SCHEDULE];
+} __attribute__((packed));
 
 
 struct schedule_t bin;
 FILE* fid;
 char output[SCRIPT_MAX_LENGTH];
-char is_exit[1];
+char is_exit;
 int i;
 int j;
 int count = 0;
 int numels = 0;
 
-void main( ) {
+int main( int argc, char** argv )
+{
+	(void) argc;
+	(void) argv;
 
 	printf("Ex-Alta 1 Flight Schedule Generator V1.0\n");
 
 	for( i = 0; i < MAX_COMMANDS_PER_SCHEDULE; i++ ) {
-		printf("This is slot %d of %d command slots\n\n", i, MAX_COMMANDS_PER_SCHEDULE);
+		printf("This is slot %d of %d command slots\n", i, MAX_COMMANDS_PER_SCHEDULE);
 
 		/* Read in unix execution time.
 		 */
 		printf("Enter unix time of command execution:\n");
 		printf("Enter a value : ");
-		scanf("%hu", &bin.slot[i].unix_time);
-		printf("Scheduling command for  %" PRIu32 "\n", bin.slot[i].unix_time);
+		scanf("%" SCNu32, &bin.slot[i].unix_time);
 		bin.slot[i].unix_time = htole32(bin.slot[i].unix_time);
 
 		/* Read in command to execute
 		 */
 		printf("Enter the command to execute using the format: \"command\", \"arg\"\n");
-		scanf(" %99[^\n]", bin.slot[i].command);
-		int copied = snprintf(output, SCRIPT_MAX_LENGTH, "COMMAND(%s);\n", bin.slot[i].command);
+		scanf(" %99[^\n\r]", bin.slot[i].command);
+		int copied = snprintf(output, SCRIPT_MAX_LENGTH, "COMMAND(%s);", bin.slot[i].command);
 		if( copied >= SCRIPT_MAX_LENGTH ) {
 			printf("Command string too large, exiting scheduler..\n");
 			return -1;
 		}
 		memcpy(bin.slot[i].command, output, strlen(output));
+		printf("Scheduling command\n\t%s @time %" PRIu32 "\n",
+		       bin.slot[i].command, bin.slot[i].unix_time);
 		bin.slot[i].command_length = strlen(bin.slot[i].command);
 
 		/* Update total commands in schedule.
@@ -83,10 +88,16 @@ void main( ) {
 		/* Exit if user has no more commands to schedule.
 		 */
 		printf("Done? Press y to exit here, n to continue\n");
-		scanf("%c", is_exit);
-		if( strcmp(is_exit,"y") == 0 ) {
+		for( ;; ) {
+			is_exit = (char) getchar();
+			if( is_exit == 'y' || is_exit == 'n' ) {
+				break;
+			}
+		}
+		if( is_exit == 'y' ) {
 			break;
 		}
+		printf("\n");
 	}
 
 	/* Write the schedule into a file for upload to S/C.
@@ -94,7 +105,7 @@ void main( ) {
 	printf("Compiling flight schedule containing %d commands:\n", le32toh(bin.total_commands));
 	fid = fopen("script.bin","w");
 	if( fid == NULL ) {
-		prinf("Failed to create file for compiled schedule\n");
+		printf("Failed to create file for compiled schedule\n");
 		return -1;
 	}
 	size_t written = fwrite(&bin, sizeof(bin), 1, fid);
